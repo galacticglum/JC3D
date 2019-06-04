02676ed17cb9619b2ed5b2ce6040a362777e182d
GalacticGlum <nerftactic09@gmail.com>
2019-06-03 23:14:38 -0400

    Added common operations

--- Engine/Source/Public/Math/Vector.h
+++ Engine/Source/Public/Math/Vector.h
@@ -13,10 +13,13 @@
 #include <initializer_list>
 #include <cassert>
 #include <array>
+#include <cmath>
 
+// Macro definition for the generic vector template definition including typename restriction to only numeric types.
 #define VECTOR_TEMPLATE template <std::size_t n, typename T, typename std::enable_if<std::is_arithmetic<T>::value>::type* = nullptr>
 
 /**
+ * \class Vector Vector.h "Public/Math/Vector.h"
  * A generic vector implementation supporting arbitrary element types.
  * 
  * @tparam n The size of the vector (dimensionality).
@@ -26,19 +29,19 @@ VECTOR_TEMPLATE
 struct Vector
 {
 	/**
-	 * The Vector element data.
+	 * The \p Vector element data.
 	 */
 	std::array<T, n> Data;
 
 	/**
-	 * A default empty constructor that initializes a new Vector: default initializes all vector elements.
+	 * A default empty constructor that initializes a new \p Vector: default initializes all vector elements.
 	 */
 	Vector() {}
 
 	/**
-	 * Initializes a new Vector with a scalar value: all vector elements are initialized to the scalar value.
+	 * Initializes a new \p Vector with a scalar value: all vector elements are initialized to the \p scalar value.
 	 * 
-	 * @param scalar The value of all the elements in this Vector.
+	 * @param scalar The value of all the elements in this \p Vector.
 	 */
 	explicit Vector(const T& scalar)
 	{
@@ -49,9 +52,9 @@ struct Vector
 	}
 
 	/**
-	 * Initializes a new Vector using an std::initializer_list<T> for accepting an arbitrary number of arguments.
+	 * Initializes a new Vector using an \p std::initializer_list<T> for accepting an arbitrary number of arguments.
 	 * 
-	 * @param args The std::initializer_list<T> used to initialize this Vector's elements.
+	 * @param args The \p std::initializer_list<T> used to initialize this Vector's elements.
 	 */
 	Vector(const std::initializer_list<T> args)
 	{
@@ -60,12 +63,7 @@ struct Vector
 	}
 
 	/**
-	 * The number of dimensions that this Vector has.
-	 */
-	static std::size_t Dimensions() { return n; }
-
-	/*
-	 * Gets a copy of this Vector with negative elements.
+	 * Gets a copy of this \p Vector with negative elements.
 	 */
 	Vector<n, T> Negative() const
 	{
@@ -79,7 +77,52 @@ struct Vector
 	}
 
 	/**
-	 * Performs an operation on each element of this Vector.
+	 * Gets the length of this \p Vector.
+	 * @note The length of a vector is defined as the square root of the square of the vector:
+	 *		 \f[|v| = \sqrt{\sum^n_{i=1} v_{i}^2},\f] where \f$v\f$ represents the vector and
+	 *		 \f$n\f$ represents its dimension. The magnitude is implemented as \f$\sqrt{v\cdot v}\f$.
+	 */
+	T Magntiude() const
+	{
+		return static_cast<T>(std::sqrt(SquareMagnitude()));
+	}
+
+	/**
+	 * Gets the squared length of this \p Vector.
+	 */
+	T SquareMagnitude() const
+	{
+		return Dot(this, this);
+	}
+
+	/**
+	 * Makes this \p Vector have a magnitude of 1.
+	 * @note This will change this Vector. If you want to keep the vector unchanged, use the \p Vector<n, T>.Normalized method instead.
+	 */
+	Vector<n, T>& Normalize()
+	{
+		T magnitude = Magntiude();
+		if (magnitude == 0) return this;
+
+		for (std::size_t i = 0; i < n; ++i)
+		{
+			Data[i] /= magnitude;
+		}
+
+		return this;
+	}
+
+	/**
+	 * Gets this \p Vector with a magnitude of 1.
+	 * @note This \p Vector remains unchanged. If you want to normalize the vector, use the \p Vector<n, T>.Normalize method instead.
+	 */
+	Vector<n, T> Normalized() const
+	{
+		return Vector<n, T>(this).Normalize();
+	}
+
+	/**
+	 * Performs an operation on each element of this \p Vector.
 	 * 
 	 * @param operation The operation to apply on each element.
 	 */
@@ -104,6 +147,56 @@ struct Vector
 	 * @param index The index of the element whose value to retrieve.
 	 */
 	const T& operator[] (const std::size_t index) const { return GetAt(index); }
+
+	/**
+	 * The number of dimensions that this Vector has.
+	 */
+	static std::size_t Dimensions() { return n; }
+
+	/**
+	 * Dot product of two vectors.
+	 * @note The dot product is a value equal to the magnitudes of the two vectors multiplied
+	 *	     together and then multiplied by the cosine of the angle between them.
+	 */
+	static T Dot(const Vector<n, T>& a, const Vector<n, T>& b)
+	{
+		T result;
+		for (std::size_t i = 0; i < n; ++i)
+		{
+			result += a[i] * b[i];
+		}
+
+		return result;
+	}
+
+	/**
+	 * Gets the distance between two vectors.
+	 * @note The distance between two vectors is the same as the magnitude of their difference.
+	 */
+	static T Distance(const Vector<n, T>& a, const Vector<n, T>&  b)
+	{
+		return (a - b).Magnitude();
+	}
+
+	/**
+	 * Gets the square distance between two vectors.
+	 * @note The square distance between two vectors is the same as the square magnitude of their difference.
+	 */
+	static T SquareDistance(const Vector<n, T>& a, const Vector<n, T>&  b)
+	{
+		return (a - b).SquareMagnitude();
+	}
+
+	/**
+	 * Linearly interpolates between two vectors.
+	 * 
+	 * Interpolates between the vectors \p a and \p y by the interpolant \y t.
+	 * The parameter \y t is clamped within the range [0, 1].
+	 */
+	static Vector<n, T> Lerp(const Vector<n, T>& a, const Vector<n, T>& b, T t)
+	{
+		return a + (b - a) * t;
+	}
 private:
 	/**
 	 * Gets the value of the element in this Vector at the specified index.
