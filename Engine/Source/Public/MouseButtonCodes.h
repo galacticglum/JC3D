90eb37194e0e9c359bb99b95559caeadfe270cab
GalacticGlum <nerftactic09@gmail.com>
2019-06-10 17:01:35 -0400

    Added mouse button codes

---
+++ Engine/Source/Public/MouseButtonCodes.h
@@ -0,0 +1,30 @@
+/**
+ * Author: Shon Verch
+ * File Name: MouseButtonCodes.h
+ * Project Name: JesusChristIn3D
+ * Creation Date: 06/10/19
+ * Modified Date: 06/10/19
+ * Description: Mouse button codes.
+ */
+
+#pragma once
+
+/**
+ * @enum MouseButton MouseButtonCodes.h
+ * @brief Mouse button codes.
+ */
+enum class MouseButton
+{
+	Button1 = 0,
+	Button2 = 1,
+	Button3 = 2,
+	Button4 = 3,
+	Button5 = 4,
+	Button6 = 5,
+	Button7 = 6,
+	Button8 = 7,
+	Last = Button8,
+	Left = Button1,
+	Right = Button2,
+	Middle = Button3,
+};
