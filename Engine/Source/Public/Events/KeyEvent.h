032759bbe0713cc4aa228847169f433660edc8ae
GalacticGlum <nerftactic09@gmail.com>
2019-06-10 10:09:43 -0400

    Updated KeyEvent.h documentation

--- Engine/Source/Public/Events/KeyEvent.h
+++ Engine/Source/Public/Events/KeyEvent.h
@@ -12,32 +12,63 @@
 #include <sstream>
 #include <Events/Event.h>
 
+/**
+ *@class KeyEvent KeyEvent.h
+ * @brief The base Event class for all keyboard related events.
+ */
 class KeyEvent : public Event
 {
 public:
-	int GetKeyCode() const { return m_KeyCode; }
-	EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput)
+	/**
+	 * @brief Get the keycode pertaining to this KeyEvent.
+	 */
+	int GetKeyCode() const
+	{
+		return m_KeyCode;
+	}
 
+	EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput)
 protected:
+	/**
+	 * @brief Initialize a new KeyEvent given a @p keycode.
+	 */
 	explicit KeyEvent(const int keycode) : m_KeyCode(keycode)
 	{
 	}
 
+	/**
+	 * @brief The keycode pertaining to this KeyEvent.
+	 */
 	int m_KeyCode;
 };
 
+/**
+ * @class KeyPressedEvent KeyEvent.h
+ * @brief Raised when a key is pressed.
+ */
 class KeyPressedEvent : public KeyEvent
 {
 public:
+	/**
+	 * @brief Initialize a new KeyPressedEvent given the @p keycode and @p repeatCount.
+	 * @param keycode The code of the key that was pressed.
+	 * @param repeatCount The number of times the key was repeated.
+	 */
 	KeyPressedEvent(const int keycode, const int repeatCount) : KeyEvent(keycode), m_RepeatCount(repeatCount)
 	{
 	}
 
+	/**
+	 * @brief Get the repeat count of the key.
+	 */
 	int GetRepeatCount() const
 	{
 		return m_RepeatCount;
 	}
 
+	/**
+	 * @brief Convert this KeyPressedEvent to its string representation.
+	 */
 	std::string ToString() const override
 	{
 		std::stringstream stream;
@@ -47,16 +78,29 @@ public:
 
 	EVENT_CLASS_TYPE(KeyPressed)
 private:
+	/**
+	 * @brief The number of times the key was repeated.
+	 */
 	int m_RepeatCount;
 };
 
+/**
+ * @class KeyReleasedEvent KeyEvent.h
+ * @brief Raised when a key is released.
+ */
 class KeyReleasedEvent : public KeyEvent
 {
 public:
+	/**
+	 * @brief Initialize a new KeyReleasedEvent given the @p keycode.
+	 */
 	explicit KeyReleasedEvent(const int keycode) : KeyEvent(keycode)
 	{
 	}
 
+	/**
+	 * @brief Convert this KeyReleasedEvent to its string representation.
+	 */
 	std::string ToString() const override
 	{
 		std::stringstream stream;
@@ -67,13 +111,30 @@ public:
 	EVENT_CLASS_TYPE(KeyReleased)
 };
 
+/**
+ * @class KeyTypedEvent KeyEvent.h
+ * @brief Raised when a key is typed.
+ * @note While this class is structurally identical to KeyEvent and
+ *		 similar to KeyPressedEvent, it is functionally very different.
+ *		 Unlike key input, key typing obeys keyboard layout and modifiers keys.
+ * @note Furthermore, the purpose of this event is fundamentally different
+ *		 to KeyPressedEvent and as such, they should not be used interchangeably.
+ *		 This event should be used for text input while KeyPressedEvent should be used
+ *		 for keyboard input.
+ */
 class KeyTypedEvent : public KeyEvent
 {
 public:
+	/**
+	 * @brief Initialize a new KeyTypedEvent given the @p keycode.
+	 */
 	explicit KeyTypedEvent(const int keycode) : KeyEvent(keycode)
 	{
 	}
 
+	/**
+	 * @Brief Convert this KeyTypedEvent to its string representation.
+	 */
 	std::string ToString() const override
 	{
 		std::stringstream stream;
