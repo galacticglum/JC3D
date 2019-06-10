5ecc73a66b4719ee5f3b120e38db27671ed88c16
GalacticGlum <nerftactic09@gmail.com>
2019-06-10 10:20:14 -0400

    Updated MouseEvent.h documentation

--- Engine/Source/Public/Events/MouseEvent.h
+++ Engine/Source/Public/Events/MouseEvent.h
@@ -13,15 +13,28 @@
 #include <Math/Vector.h>
 #include <sstream>
 
+/**
+ * @class MouseMovedEvent MouseEvent.h
+ * @brief Raised when the mouse cursor moves.
+ */
 class MouseMovedEvent : public Event
 {
 public:
+	/**
+	 * @brief The new position of the mouse.
+	 */
+	const Vector2f Position;
+
+	/**
+	 * @brief Initialize a new MouseMovedEvent given its new @p position.
+	 */
 	explicit MouseMovedEvent(const Vector2f position) : Position(position)
 	{
 	}
 
-	const Vector2f Position;
-
+	/**
+	 * @brief Convert this MouseEvent to its string representation.
+	 */
 	std::string ToString() const override
 	{
 		std::stringstream stream;
@@ -33,15 +46,28 @@ public:
 	EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
 };
 
+/**
+ * @class MouseScrolledEvent MouseEvent.h
+ * @brief Raised when the user scrolls via the mouse.
+ */
 class MouseScrolledEvent : public Event
 {
 public:
-	MouseScrolledEvent(const Vector2f offset) : Offset(offset)
+	/**
+	 * @brief The scroll offsets.
+	 */
+	const Vector2f Offset;
+
+	/**
+	 * @brief Initialize a new MouseScrolledEvent given the scroll @offset.
+	 */
+	explicit MouseScrolledEvent(const Vector2f offset) : Offset(offset)
 	{
 	}
 
-	const Vector2f Offset;
-
+	/**
+	 * @brif Convert this MouseScrolledEvent to its string representation.
+	 */
 	std::string ToString() const override
 	{
 		std::stringstream stream;
@@ -53,9 +79,16 @@ public:
 	EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
 };
 
+/**
+ * @class MouseButtonEvent MouseEvent.h
+ * @brief The base Event class for all events related to mouse buttons.
+ */
 class MouseButtonEvent : public Event
 {
 public:
+	/**
+	 * @brief Gets the mouse button pertaining to this MouseButtonEvent.
+	 */
 	int GetMouseButton() const
 	{
 		return m_Button;
@@ -63,15 +96,36 @@ public:
 
 	EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
 protected:
-	explicit MouseButtonEvent(const int button) : m_Button(button) {}
+	/**
+	 * @brief Initialize a new MouseButtonEvent given the @p button.
+	 */
+	explicit MouseButtonEvent(const int button) : m_Button(button)
+	{
+	}
 
+	/**
+	 * @brief The mouse button pertaining to this MouseButtonEvent.
+	 */
 	int m_Button;
 };
 
+/**
+ * @class MouseButtonPressedEvent MouseEvent.h
+ * @brief Raised when a mouse button is pressed.
+ */
 class MouseButtonPressedEvent : public MouseButtonEvent
 {
 public:
-	explicit MouseButtonPressedEvent(const int button) : MouseButtonEvent(button) {}
+	/**
+	 * @brief Initialize a new MouseButtonPressedEvent given the @p button.
+	 */
+	explicit MouseButtonPressedEvent(const int button) : MouseButtonEvent(button)
+	{
+	}
+
+	/**
+	 * @brief Converts this MouseButtonPressedEvent to its string representation.
+	 */
 	std::string ToString() const override
 	{
 		std::stringstream stream;
@@ -82,10 +136,23 @@ public:
 	EVENT_CLASS_TYPE(MouseButtonPressed)
 };
 
+/**
+ * @class MouseButtonReleasedEvent MouseEvent.h
+ * @brief Raised when a mouse button is released.
+ */
 class MouseButtonReleasedEvent : public MouseButtonEvent
 {
 public:
-	explicit MouseButtonReleasedEvent(const int button) : MouseButtonEvent(button) {}
+	/**
+	 * @brief Initialize a new MouseButtonReleasedEvent given the @p button.
+	 */
+	explicit MouseButtonReleasedEvent(const int button) : MouseButtonEvent(button)
+	{
+	}
+
+	/**
+	 * @brief Converts this MouseButtonReleasedEvent to its string representation.
+	 */
 	std::string ToString() const override
 	{
 		std::stringstream ss;
