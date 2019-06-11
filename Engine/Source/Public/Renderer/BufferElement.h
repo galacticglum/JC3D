d39e66f7406da368b483f96c05f7ec4f021d65b6
GalacticGlum <nerftactic09@gmail.com>
2019-06-10 23:39:17 -0400

    Added BufferElement

---
+++ Engine/Source/Public/Renderer/BufferElement.h
@@ -0,0 +1,96 @@
+/**
+ * Author: Shon Verch
+ * File Name: BufferElement.h
+ * Project Name: JesusChristIn3D
+ * Creation Date: 06/10/2019
+ * Modified Date: 06/10/2019
+ * Description: An individual element of a BufferLayout.
+ */
+
+#pragma once
+
+#include <utility>
+
+#include <Logger.h>
+
+/**
+ * @enum ShaderDataType BufferElement.h
+ * @brief The different data types in a shader.
+ */
+enum class ShaderDataType
+{
+	None = 0,
+	Float,
+	Int,
+	Bool,
+	Vector2f,
+	Vector3f,
+	Vector4f,
+	Vector2i,
+	Vector3i,
+	Vector4i,
+	Matrix3f,
+	Matrix4f,
+};
+
+/**
+ * @brief Gets the size of the specified @p type.
+ */
+static std::size_t ShaderDataTypeSize(ShaderDataType type)
+{
+	constexpr int FLOAT_SIZE = 4;
+	constexpr int INT_SIZE  = 4;
+
+	switch(type)
+	{
+		case ShaderDataType::Float: return FLOAT_SIZE;
+		case ShaderDataType::Int: return INT_SIZE;
+		case ShaderDataType::Bool: return 1;
+		case ShaderDataType::Vector2f: return FLOAT_SIZE * 2;
+		case ShaderDataType::Vector3f: return FLOAT_SIZE * 3;
+		case ShaderDataType::Vector4f: return FLOAT_SIZE * 4;
+		case ShaderDataType::Vector2i: return INT_SIZE * 2;
+		case ShaderDataType::Vector3i: return INT_SIZE * 3;
+		case ShaderDataType::Vector4i: return INT_SIZE * 4;
+		case ShaderDataType::Matrix3f: return FLOAT_SIZE * 3 * 3;
+		case ShaderDataType::Matrix4f: return FLOAT_SIZE * 4 * 4;
+	}
+
+	LOG_CATEGORY_ASSERT(false, "Renderer", "Unknown ShaderDataType!");
+	return 0;
+}
+
+ /**
+  * @struct BufferElement BufferElement.h
+  * @brief An individual element of a BufferLayout.
+  */
+struct BufferElement
+{
+	/**
+	 * @brief The name of this BufferElement.
+	 */
+	std::string Name;
+
+	/**
+	 * @brief The offset of this BufferElement in the buffer.
+	 */
+	uint32_t Offset;
+
+	/**
+	 * @brief The size of this BufferElement.
+	 */
+	std::size_t Size;
+
+	/**
+	 * @brief The data type of this BufferElement.
+	 */
+	ShaderDataType Type;
+
+	/**
+	 * @brief Initialize a new BufferElement.
+	 */
+	BufferElement(const ShaderDataType type, std::string name) : 
+		Name(std::move(name)), Offset(0), Size(ShaderDataTypeSize(type)), Type(type)
+	{
+	}
+};
