fb1e39d2b6c485e07dbadf07c44217275f5cc414
GalacticGlum <nerftactic09@gmail.com>
2019-06-12 16:34:20 -0400

    Updated OpenGL implementation of Texture2D

--- Engine/Source/Private/Platform/OpenGL/OpenGLTexture2D.cpp
+++ Engine/Source/Private/Platform/OpenGL/OpenGLTexture2D.cpp
@@ -1,127 +1,129 @@
 #include <Platform/OpenGL/OpenGLTexture2D.h>
-#include <Utilities/ImageLoad.h>
+#include <Renderer/Renderer.h>
 #include <Logger.h>
 
 #include <utility>
 #include <glad/glad.h>
 
-OpenGLTexture2D::OpenGLTexture2D(const uint32_t width, const uint32_t height, const TextureParameters parameters,
-	const TextureLoadOptions loadOptions) : m_FilePath("NULL"), m_Width(width), m_Height(height), 
-											m_Parameters(parameters), m_LoadOptions(loadOptions)
-{
-	m_TextureId = Load();
-}
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
 
-OpenGLTexture2D::OpenGLTexture2D(std::string filepath, const TextureParameters parameters,
-	const TextureLoadOptions loadOptions) : m_FilePath(std::move(filepath)), m_Parameters(parameters), 
-											m_LoadOptions(loadOptions)
-{
-	m_TextureId = Load();
-}
-
-OpenGLTexture2D::~OpenGLTexture2D()
-{
-	glDeleteTextures(1, &m_TextureId);
-}
-
-void OpenGLTexture2D::Bind(const uint32_t slot) const
-{
-	glActiveTexture(GL_TEXTURE0 + slot);
-	glBindTexture(GL_TEXTURE_2D, m_TextureId);
-}
-
-void OpenGLTexture2D::Unbind(const uint32_t slot) const
-{
-	glActiveTexture(GL_TEXTURE0 + slot);
-	glBindTexture(GL_TEXTURE_2D, 0);
-}
-
-void OpenGLTexture2D::SetData(const void* pixels)
-{
-	glBindTexture(GL_TEXTURE_2D, m_TextureId);
-	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, m_Width, m_Height, TextureFormatToGL(m_Parameters.Format), GL_UNSIGNED_BYTE, pixels);
-}
-
-uint32_t OpenGLTexture2D::Load()
-{
-	unsigned char* pixels = nullptr;
-	if (m_FilePath != "NULL")
-	{
-		uint32_t channels;
-		pixels = Image::Load(m_FilePath, &m_Width, &m_Height, &channels, m_LoadOptions.FlipY);
-		if (channels != 1 && channels != 3 && channels != 4)
-		{
-			Logger::Log("Renderer", LoggerVerbosity::Error, "Unsupported image bit-depth! (channels: {0}, filepath: {1})!", channels, m_FilePath);
-		}
-
-		if (channels == 1)
-		{
-			m_Parameters.Format = TextureFormat::Luminance;
-		}
-		else if (channels == 3)
-		{
-			m_Parameters.Format = TextureFormat::RGB;
-		}
-		else if (channels == 4)
-		{
-			m_Parameters.Format = TextureFormat::RGBA;
-		}
-	}
-
-	uint32_t textureId;
-	glGenTextures(1, &textureId);
-	glBindTexture(GL_TEXTURE_2D, textureId);
-
-	// Setup the texture parameters based on our texture parameter info.
-	const GLenum minFilter = m_Parameters.Filter == TextureFilter::Linear ? GL_LINEAR_MIPMAP_LINEAR : GL_NEAREST;
-	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilter);
-
-	const GLenum magFilter = m_Parameters.Filter == TextureFilter::Linear ? GL_LINEAR : GL_NEAREST;
-	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilter);
-
-	const GLenum wrap = TextureWrapToGL(s_WrapMode);
-	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap);
-	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrap);
-
-	const GLenum format = TextureFormatToGL(m_Parameters.Format);
-	glTexImage2D(GL_TEXTURE_2D, 0, format, m_Width, m_Height, 0, format, GL_UNSIGNED_BYTE, pixels ? pixels : nullptr);
-
-	glGenerateMipmap(GL_TEXTURE_2D);
-
-	// Unbind the texture since we are done.
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	// Free the pixels now that we are done with them.
-	// Since we have our texture object, we don't need the raw data.
-	delete pixels;
-
-
-	return textureId;
-}
-
-uint32_t OpenGLTexture2D::TextureFormatToGL(const TextureFormat format)
+/**
+ * @brief Convert the TextureFormat to OpenGL.
+ */
+static GLenum TextureFormatToGL(const TextureFormat format)
 {
 	switch (format)
 	{
 		case TextureFormat::RGB: return GL_RGB;
 		case TextureFormat::RGBA: return GL_RGBA;
-		case TextureFormat::Luminance: return GL_RED;
-		case TextureFormat::LuminanceAlpha: return GL_RG;
 	}
 
 	return 0;
 }
 
-uint32_t OpenGLTexture2D::TextureWrapToGL(const TextureWrap wrap)
+/**
+ * @brief Calculates mipmap count from @p width and @p height.
+ */
+static int CalculateMipMapCount(const int width, const int height)
 {
-	switch (wrap)
+	int levels = 1;
+	while ((width | height) >> levels)
 	{
-		case TextureWrap::Repeat: return GL_REPEAT;
-		case TextureWrap::Clamp: return GL_CLAMP_TO_EDGE;
-		case TextureWrap::MirroredRepeat: return GL_MIRRORED_REPEAT;
-		case TextureWrap::ClampToEdge: return GL_CLAMP_TO_EDGE;
-		case TextureWrap::ClampToBorder: return GL_CLAMP_TO_BORDER;
+		levels++;
 	}
 
-	return 0;
+	return levels;
+}
+
+OpenGLTexture2D::OpenGLTexture2D(const TextureFormat format, const uint32_t width, const uint32_t height)
+	: m_Width(width), m_Height(height), m_TextureFormat(format)
+{
+	OpenGLTexture2D* self = this;
+	ENGINE_RENDER_1(self, {
+		glGenTextures(1, &self->m_TextureId);
+		glBindTexture(GL_TEXTURE_2D, self->m_TextureId);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTextureParameterf(self->m_TextureId, GL_TEXTURE_MAX_ANISOTROPY, RendererAPI::GetCapabilities().MaxAnisotropy);
+
+		glTexImage2D(GL_TEXTURE_2D, 0, TextureFormatToGL(self->m_TextureFormat), self->m_Width, 
+			self->m_Height, 0, TextureFormatToGL(self->m_TextureFormat), GL_UNSIGNED_BYTE, nullptr);
+
+		glGenerateMipmap(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, 0);
+	});
+}
+
+OpenGLTexture2D::OpenGLTexture2D(std::string filepath,  bool srgb)
+	: m_FilePath(std::move(filepath)), m_TextureId(0)
+{
+	int width, height, channels;
+	Logger::Log("Renderer", LoggerVerbosity::Info, "Loading texture \"{0}\" (srgb = {1})", filepath, srgb);
+	m_ImageData = stbi_load(filepath.c_str(), &width, &height, &channels, srgb ? STBI_rgb : STBI_rgb_alpha);
+	m_Width = width;
+	m_Height = height;
+	m_TextureFormat = TextureFormat::RGBA;
+
+	ENGINE_RENDER_S1(srgb, {
+		if (srgb)
+		{
+			glCreateTextures(GL_TEXTURE_2D, 1, &self->m_TextureId);
+			const int levels = CalculateMipMapCount(self->m_Width, self->m_Height);
+			Logger::Log("Renderer", LoggerVerbosity::Info, "Creating srgb texture width {0} mips", levels);
+
+			glTextureStorage2D(self->m_TextureId, levels, GL_SRGB8, self->m_Width, self->m_Height);
+			glTextureParameteri(self->m_TextureId, GL_TEXTURE_MIN_FILTER, levels > 1 ? GL_LINEAR_MIPMAP_LINEAR : GL_LINEAR);
+			glTextureParameteri(self->m_TextureId, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+			glTextureSubImage2D(self->m_TextureId, 0, 0, 0, self->m_Width, self->m_Height,
+				GL_RGB, GL_UNSIGNED_BYTE, self->m_ImageData);
+
+			glGenerateTextureMipmap(self->m_TextureId);
+		}
+		else
+		{
+			glGenTextures(1, &self->m_TextureId);
+			glBindTexture(GL_TEXTURE_2D, self->m_TextureId);
+
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+			glTexImage2D(GL_TEXTURE_2D, 0, TextureFormatToGL(self->m_TextureFormat), self->m_Width, self->m_Height, 
+				0,  srgb ? GL_SRGB8 : TextureFormatToGL(self->m_TextureFormat), GL_UNSIGNED_BYTE, self->m_ImageData);
+
+			glGenerateMipmap(GL_TEXTURE_2D);
+			glBindTexture(GL_TEXTURE_2D, 0);
+		}
+
+		stbi_image_free(self->m_ImageData);
+	});
+}
+
+OpenGLTexture2D::~OpenGLTexture2D()
+{
+	OpenGLTexture2D* self = this;
+	ENGINE_RENDER_1(self, {
+		glDeleteTextures(1, &self->m_TextureId);
+	});
+}
+
+void OpenGLTexture2D::Bind(const uint32_t slot) const
+{
+	ENGINE_RENDER_S1(slot, {
+			glBindTextureUnit(slot, self->m_TextureId);
+	});
+}
+
+void OpenGLTexture2D::Unbind(const uint32_t slot) const
+{
+	ENGINE_RENDER_1(slot, {
+		glActiveTexture(GL_TEXTURE0 + slot);
+		glBindTexture(GL_TEXTURE_2D, 0);
+	});
 }
--- Engine/Source/Public/Platform/OpenGL/OpenGLTexture2D.h
+++ Engine/Source/Public/Platform/OpenGL/OpenGLTexture2D.h
@@ -19,18 +19,13 @@
 class OpenGLTexture2D : public Texture2D
 {
 public:
-	OpenGLTexture2D(uint32_t width, uint32_t height, TextureParameters parameters = TextureParameters(), 
-		TextureLoadOptions loadOptions = TextureLoadOptions());
-
-	explicit OpenGLTexture2D(std::string filepath, TextureParameters parameters = TextureParameters(), 
-		TextureLoadOptions loadOptions = TextureLoadOptions());
+	OpenGLTexture2D(TextureFormat format, uint32_t width, uint32_t height);
+	explicit OpenGLTexture2D(std::string filepath, bool srgb);
 
 	~OpenGLTexture2D();
 
 	void Bind(uint32_t slot) const override;
 	void Unbind(uint32_t slot) const override;
-
-	void SetData(const void* pixels) override;
 
 	uint32_t GetWidth() const override
 	{
@@ -47,23 +42,21 @@ public:
 		return m_FilePath;
 	}
 
-	uint32_t GetHandle() const
+	uint32_t GetHandle() const override
 	{
 		return m_TextureId;
 	}
 
+	TextureFormat GetFormat() const override
+	{
+		return m_TextureFormat;
+	}
 private:
-	std::string m_FilePath;
-	
-	uint32_t m_TextureId;
 	uint32_t m_Width{};
 	uint32_t m_Height{};
+	TextureFormat m_TextureFormat;
+	uint32_t m_TextureId{};
 
-	TextureParameters m_Parameters;
-	TextureLoadOptions m_LoadOptions;
-
-	uint32_t Load();
-
-	static uint32_t TextureFormatToGL(TextureFormat format);
-	static uint32_t TextureWrapToGL(TextureWrap wrap);
+	unsigned char* m_ImageData;
+	std::string m_FilePath;
 };
