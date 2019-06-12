e9dcf0144b00681de72baf51524c7d18dfb2baba
GalacticGlum <nerftactic09@gmail.com>
2019-06-12 16:34:46 -0400

    Added Renderer.cpp

--- Engine/Source/Private/Renderer/Renderer.cpp
+++ Engine/Source/Private/Renderer/Renderer.cpp
@@ -1,3 +1,40 @@
 #include <Renderer/Renderer.h>
 
-RendererAPI Renderer::s_RendererAPI = RendererAPI::OpenGL;
+Renderer* Renderer::s_Instance = new Renderer();
+RendererAPIType RendererAPI::s_CurrentRendererAPI = RendererAPIType::OpenGL;
+
+void Renderer::Initialize()
+{
+	ENGINE_RENDER({ RendererAPI::Initialize(); });
+}
+
+void Renderer::Clear()
+{
+	ENGINE_RENDER({ RendererAPI::Clear(0.0f, 0.0f, 0.0f, 1.0f); });
+}
+
+void Renderer::Clear(float r, float g, float b, float a)
+{
+	ENGINE_RENDER_4(r, g, b, a, { RendererAPI::Clear(r, g, b, a); });
+}
+
+void Renderer::ClearMagenta()
+{
+	Clear(1, 0, 1);
+}
+
+void Renderer::SetClearColor(float r, float g, float b, float a)
+{
+}
+
+void Renderer::DrawIndexed(unsigned int count, bool depthTest)
+{
+	ENGINE_RENDER_2(count, depthTest, {
+		RendererAPI::DrawIndexed(count, depthTest);
+	});
+}
+
+void Renderer::WaitAndRender()
+{
+	s_Instance->m_CommandQueue.Execute();
+}
