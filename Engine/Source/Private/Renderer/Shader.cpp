63aedc80b81a8695b42e252b60d73d477a43043d
GalacticGlum <nerftactic09@gmail.com>
2019-06-12 16:37:06 -0400

    Updated Shader

--- Engine/Source/Private/Renderer/Shader.cpp
+++ Engine/Source/Private/Renderer/Shader.cpp
@@ -6,12 +6,12 @@
 
 Shader* Shader::Create(const std::string& vertexPath, const std::string& fragmentPath, const std::string& geometryPath)
 {
-	switch (Renderer::GetAPI())
+	switch (RendererAPI::Current())
 	{
-		case RendererAPI::None:
+		case RendererAPIType::None:
 			LOG_CATEGORY_ASSERT(false, "Renderer", "RendererAPI::None is not supported!");
 			return nullptr;
-		case RendererAPI::OpenGL:
+		case RendererAPIType::OpenGL:
 			return new OpenGLShader(vertexPath, fragmentPath, geometryPath);
 	}
 
