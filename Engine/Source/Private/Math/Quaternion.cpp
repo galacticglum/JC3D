4b69209c1428daf6015c0d250358814a88c16e34
GalacticGlum <nerftactic09@gmail.com>
2019-06-13 00:07:33 -0400

    Completed Quaternion implementation

--- Engine/Source/Private/Math/Quaternion.cpp
+++ Engine/Source/Private/Math/Quaternion.cpp
@@ -3,7 +3,231 @@
 #include <Math/MathFunctions.h>
 #include <cmath>
 
-Quaternion Quaternion::AxisAngle(const Vector<3, float>& axis, float angle, const bool radians)
+Vector<3, float> Quaternion::GetXYZ() const
+{
+	return Vector<3, float>(X, Y, Z);
+}
+
+Quaternion& Quaternion::SetXYZ(const Vector<3, float>& vector)
+{
+	X = vector.X;
+	Y = vector.Y;
+	Z = vector.Z;
+	return *this;
+}
+
+float Quaternion::Magnitude() const
+{
+	return std::sqrtf(SquareMagnitude());
+}
+
+float Quaternion::SquareMagnitude() const
+{
+	return X * X + Y * Y + Z * Z + W * W;
+}
+
+Quaternion Quaternion::Negative() const
+{
+	return Quaternion(-X, -Y, -Z, -W);
+}
+
+Quaternion Quaternion::Normalized() const
+{
+	const float length = Magnitude();
+	return Quaternion(X / length, Y / length, Z / length, W / length);
+}
+
+Quaternion Quaternion::Conjugate() const
+{
+	return Quaternion(-X, -Y, -Z, W);
+}
+
+Vector<3, float> Quaternion::GetAxis() const
+{
+	const float x = 1 - W * W;
+
+	// Check so we don't divide by zero!
+	if (x < MathFunctions::Epsilon)
+	{
+		return Vector<3, float>::XAxis;
+	}
+
+	return GetXYZ() / (x * x);
+}
+
+Vector<3, float> Quaternion::ToEulerAngles() const
+{
+	const float x1 = 2 * X * W - 2 * Y * Z;
+	const float x2 = 1 - 2 * X * X - 2 * Z * Z;
+	const float y1 = 2 * Y * W - 2 * X * Z;
+	const float y2 = 1 - 2 * Y * Y - 2 * Z * Z;
+	const float z = 2 * X * Y + 2 * Z * W;
+
+	return Vector<3, float>(std::atan2f(x1, x2), std::atan2f(y1, y2), std::asinhf(z));
+}
+
+#define QUATERNION_COMPONENT_WISE_OP_COPY(x, op) (Quaternion(X op x.X, Y op x.Y, Z op x.Z, W op x.W))
+#define QUATERNION_SCALAR_OP_COPY(x, op) (Quaternion(X op x, Y op x, Z op x, W op x))
+
+Quaternion Quaternion::operator+(const Quaternion& quaternion) const
+{
+	return QUATERNION_COMPONENT_WISE_OP_COPY(quaternion, +);
+}
+
+Quaternion Quaternion::operator-(const Quaternion& quaternion) const
+{
+	return QUATERNION_COMPONENT_WISE_OP_COPY(quaternion, -);
+}
+
+Quaternion Quaternion::operator*(const Quaternion& quaternion) const
+{
+	return Quaternion(
+		(((W * quaternion.X) + (X * quaternion.W)) + (Y * quaternion.Z)) - (Z * quaternion.Y),
+		(((W * quaternion.Y) + (Y * quaternion.W)) + (Z * quaternion.X)) - (X * quaternion.Z),
+		(((W * quaternion.Z) + (Z * quaternion.W)) + (X * quaternion.Y)) - (Y * quaternion.X),
+		(((W * quaternion.W) - (X * quaternion.X)) - (Y * quaternion.Y)) - (Z * quaternion.Z)
+	);
+}
+
+Quaternion Quaternion::operator*(const float scalar) const
+{
+	return QUATERNION_SCALAR_OP_COPY(scalar, *);
+}
+
+Quaternion Quaternion::operator/(const float scalar) const
+{
+	return QUATERNION_SCALAR_OP_COPY(scalar, /);
+}
+
+float Quaternion::operator[](const uint32_t index) const
+{
+	LOG_ASSERT(index >= 0 && index < 4, "Index out of bounds!");
+	switch (index)
+	{
+		case 0: return X;
+		case 1: return Y;
+		case 2: return Z;
+		case 3: return W;
+	}
+
+	// We should never reach here due to the assert
+	return 0;
+}
+
+Quaternion Quaternion::operator-() const
+{
+	return this->Negative();
+}
+
+bool Quaternion::operator==(const Quaternion& quaternion) const
+{
+	return (X == quaternion.X) && (Y == quaternion.Y) && (Z == quaternion.Z) && (W == quaternion.W);
+}
+
+bool Quaternion::operator!=(const Quaternion& quaternion) const
+{
+	return !(*this == quaternion);
+}
+
+Vector<3, float> Quaternion::Rotate(const Quaternion& quaternion, const Vector<3, float>& vector)
+{
+	const float x = (((quaternion.W * vector.X) + (quaternion.Y * vector.Z)) - (quaternion.Z * vector.Y));
+	const float y = (((quaternion.W * vector.Y) + (quaternion.Z * vector.X)) - (quaternion.X * vector.Z));
+	const float z = (((quaternion.W * vector.Z) + (quaternion.X * vector.Y)) - (quaternion.Y * vector.X));
+	const float w = (((quaternion.X * vector.X) + (quaternion.Y * vector.Y)) + (quaternion.Z * vector.Z));
+
+	return Vector<3, float>(
+		((((w * quaternion.X) + (x * quaternion.W)) - (y * quaternion.Z)) + (z * quaternion.Y)),
+		((((w * quaternion.Y) + (y * quaternion.W)) - (z * quaternion.X)) + (x * quaternion.Z)),
+		((((w * quaternion.Z) + (z * quaternion.W)) - (x * quaternion.Y)) + (y * quaternion.X))
+	);
+}
+
+Quaternion& Quaternion::Normalize(Quaternion& quaternion)
+{
+	quaternion /= quaternion.Magnitude();
+	return quaternion;
+}
+
+float Quaternion::Dot(const Quaternion& a, const Quaternion& b)
+{
+	float result = (a.X * b.X);
+	result += (a.Y * b.Y);
+	result += (a.Z * b.Z);
+	result += (a.W * b.W);
+
+	return result;
+}
+
+Quaternion Quaternion::FromEulerAngles(const Vector<3, float>& angles)
+{
+	const Quaternion pitch(Vector3f::XAxis, angles.X);
+	const Quaternion yaw(Vector3f::YAxis, angles.Y);
+	const Quaternion roll(Vector3f::ZAxis, angles.Z);
+
+	return pitch * yaw * roll;
+}
+
+Quaternion Quaternion::Lerp(const Quaternion& a, const Quaternion& b, float t, bool shortest)
+{
+	const Quaternion correctedStart = a.Normalized();
+	Quaternion correctDestination = b;
+
+	if (shortest && Dot(a, b) < 0)
+	{
+		correctDestination = b.Negative();
+	}
+
+	return Quaternion(MathFunctions::Lerp(correctedStart.X, correctDestination.X, t),
+		MathFunctions::Lerp(correctedStart.Y, correctDestination.Y, t),
+		MathFunctions::Lerp(correctedStart.Z, correctDestination.Z, t),
+		MathFunctions::Lerp(correctedStart.W, correctDestination.W, t));
+}
+
+Quaternion Quaternion::Slerp(const Quaternion& a, const Quaternion& b, float t, const bool shortest)
+{
+	float destinationDot = Dot(b, b);
+	Quaternion correctDestination = b;
+
+	if (shortest && destinationDot < 0)
+	{
+		destinationDot = -destinationDot;
+		correctDestination = b.Negative();
+	}
+
+	if (std::abs(destinationDot) >= 1 - MathFunctions::Epsilon)
+	{
+		return Lerp(a, correctDestination, t, false);
+	}
+
+	const float sqrtDest = std::sqrtf(1.0f - destinationDot * destinationDot);
+	const float angle = std::atan2f(sqrtDest, destinationDot);
+	const float invertedDest = 1.0f / sqrtDest;
+
+	const float factor = std::sinf((1.0f - t) * angle) * invertedDest;
+	const float destinationFactor = std::sinf(t * angle) * invertedDest;
+
+	return (a * factor) + (correctDestination * destinationFactor);
+}
+
+std::string Quaternion::ToString() const
+{
+	return "(" + std::to_string(X) + ", " + std::to_string(Y) + ", " + std::to_string(Z) + ", " + std::to_string(W) + ")";
+}
+
+std::ostream& operator<<(std::ostream& stream, const Quaternion& right)
+{
+	return stream << right.ToString();
+}
+
+Quaternion Quaternion::Rotation(const Vector<3, float>& directionA, const Vector<3, float>& directionB)
+{
+	const float cosHalfAngleX2 = std::sqrt((2.0f * (1.0f + Vector3f::Dot(directionA, directionB))));
+	const float reciprocalCosHlfAngleX2 = 1.0f / cosHalfAngleX2;
+	return Quaternion(Vector3f::Cross(directionA, directionB) * reciprocalCosHlfAngleX2, cosHalfAngleX2 * 0.5f);
+}
+
+Quaternion Quaternion::Rotation(const Vector<3, float>& axis, float angle, const bool radians)
 {
 	if (!radians)
 	{
--- Engine/Source/Public/Math/Quaternion.h
+++ Engine/Source/Public/Math/Quaternion.h
@@ -45,6 +45,14 @@ struct Quaternion
 	}
 
 	/**
+	 * @brief Initializes a new Quaternion from a @p scalar value.
+	 * @Note All components are set to the specified @p scalar value.
+	 */
+	explicit Quaternion(const float scalar) : Quaternion(scalar, scalar, scalar, scalar)
+	{
+	}
+
+	/**
 	 * @brief Initializes a new Quaternion given the components.
 	 * @param x The x-component of this Quaternion.
 	 * @param y The y-component of this Quaternion.
@@ -65,22 +73,207 @@ struct Quaternion
 	}
 
 	/**
+	 * @brief Get the vector part of this Quaternion.
+	 */
+	Vector<3, float> GetXYZ() const;
+
+	/**
+	 * @brief Set the vector part of this Quaternion.
+	 */
+	Quaternion& SetXYZ(const Vector<3, float>& vector);
+
+	/**
+	 * @brief Gets the magnitude of this Quaternion.
+	 */
+	float Magnitude() const;
+
+	/**
+	 * @brief Gets the square magnitude of this Quaternion.
+	 */
+	float SquareMagnitude() const;
+
+	/**
+	 * @brief Gets a copy of this Quaternion with negative elements.
+	 * @returns A copy of this Quaternion whose elements have been negated.
+	 */
+	Quaternion Negative() const;
+
+	/**
 	 * @brief Gets this Quaternion with a magnitude of 1.
 	 */
 	Quaternion Normalized() const;
 
 	/**
-	 * @brief Gets the conjugsate of this Quaternion.
+	 * @brief Gets the conjugate of this Quaternion.
 	 */
 	Quaternion Conjugate() const;
 
 	/**
-	 * @brief Initializes a new Quaternion from an @p axis and @p angle.
+	 * @brief Gets the axis of this Quaternion.
+	 */
+	Vector<3, float> GetAxis() const;
+
+	/**
+	 * @brief Gets this Quaternion as euler angles.
+	 */
+	Vector<3, float> ToEulerAngles() const;
+
+	/**
+	 * @brief Assignment operator.
+	 */
+	Quaternion& operator=(const Quaternion& quaternion) = default;
+
+	/**
+	 * @brief Add two Quaternions together.
+	 */
+	Quaternion operator+(const Quaternion& quaternion) const;
+
+	/**
+	 * @brief Subtract two Quaternions.
+	 */
+	Quaternion operator-(const Quaternion& quaternion) const;
+
+	/**
+	 * @brief Multiply two Quaternions.
+	 */
+	Quaternion operator*(const Quaternion& quaternion) const;
+
+	/**
+	 * @brief Multiply a Quaternion by a scalar value.
+	 */
+	Quaternion operator*(float scalar) const;
+
+	/**
+	 * @brief Divide a Quaternion by a scalar value.
+	 */
+	Quaternion operator/(float scalar) const;
+
+	/**
+	 * @brief Index operator.
+	 */
+	float operator[](uint32_t index) const;
+
+	/**
+	 * @brief Add a Quaternion to this Quaternion.
+	 */
+	Quaternion& operator+=(const Quaternion& quaternion)
+	{
+		*this = *this + quaternion;
+		return *this;
+	}
+
+	/**
+	 * @brief Subtract a Quaternion from this Quaternion.
+	 */
+	Quaternion& operator-=(const Quaternion& quaternion)
+	{
+		*this = *this - quaternion;
+		return *this;
+	}
+
+	/**
+	 * @brief Multiply this Quaternion by a Quaternion.
+	 */
+	Quaternion& operator*=(const Quaternion& quaternion)
+	{
+		*this = *this * quaternion;
+		return *this;
+	}
+
+	/**
+	 * @brief Multiply this Quaternion by a scalar value.
+	 */
+	Quaternion& operator*=(const float scalar)
+	{
+		*this = *this * scalar;
+		return *this;
+	}
+
+	/**
+	 * @brief Divide this Quaternion by a scalar value.
+	 */
+	Quaternion& operator/=(const float scalar)
+	{
+		*this = *this / scalar;
+		return *this;
+	}
+
+	/**
+	 * @brief Negate operator.
+	 */
+	Quaternion operator-() const;
+
+	/**
+	 * @brief Determine whether a Quaternion is equal to this Quaternion.
+	 */
+	bool operator==(const Quaternion& quaternion) const;
+
+	/**
+	 * @brief Determine whether a Quaternion is not equal to this Quaternion.
+	 */
+	bool operator!=(const Quaternion& quaternion) const;
+
+	/**
+	 * @brief Rotate a quaternion by a vector.
+	 */
+	static Vector<3, float> Rotate(const Quaternion& quaternion, const Vector<3, float>& vector);
+
+	/**
+	 * @brief Create a rotation from the two specified direction vectors.
+	 */
+	static Quaternion Rotation(const Vector<3, float>& directionA, const Vector<3, float>& directionB);
+
+	/**
+	 * @brief Create a rotation from an @p axis and @p angle.
 	 * @param axis The rotation axis.
 	 * @param angle The angle about the @p axis.
 	 * @param radians A boolean indicating whether or not the specified @p angle is in radians.
 	 *				  If true, @p angle is in radians; otherwise, @p angle is in degrees.
 	 *				  This value defaults to true.
 	 */
-	static Quaternion AxisAngle(const Vector<3, float>& axis, float angle, bool radians = true);
+	static Quaternion Rotation(const Vector<3, float>& axis, float angle, bool radians = true);
+
+	/**
+	 * @brief Makes the specified @p quaternion have a magnitude of 1.
+	 * @note This will change this Quaternion. If you want to keep the quaternion unchanged,
+	 *		 use the Quaternion::Normalized method instead.
+	 * @returns A reference to @p quaternion.
+	 */
+	static Quaternion& Normalize(Quaternion& quaternion);
+
+	/**
+	 * @brief Dot product of two quaternions.
+	 * @returns The dot product of quaternion @p a and @p b.
+	 */
+	static float Dot(const Quaternion& a, const Quaternion& b);
+
+	/**
+	 * @brief Create a Quaternion from Euler angles.
+	 */
+	static Quaternion FromEulerAngles(const Vector<3, float>& angles);
+
+	/**
+	 * @brief Linear interpolation between two Quaternions by the interpolant.
+	 */
+	static Quaternion Lerp(const Quaternion& a, const Quaternion& b, float t, bool shortest);
+
+	/**
+	 * @brief Spherical interpolation between two Quaternions by the interpolant.
+	 */
+	static Quaternion Slerp(const Quaternion& a, const Quaternion& b, float t, bool shortest);
+
+	/**
+	 * @brief Convert this Quaternion to its string representation.
+	 */
+	std::string ToString() const;
+
+	/**
+	 * @brief Stream operator.
+	 */
+	friend std::ostream& operator<<(std::ostream& stream, const Quaternion& right);
+
+	/**
+	 * @brief The identity Quaternion.
+	 */
+	static const Quaternion Identity;
 };
