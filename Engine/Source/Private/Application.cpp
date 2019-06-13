179d36fca88d2ddd278f228321f057bd4a53c16e
GalacticGlum <nerftactic09@gmail.com>
2019-06-13 00:07:17 -0400

    Added OpenFile dialog to Application

--- Engine/Source/Private/Application.cpp
+++ Engine/Source/Private/Application.cpp
@@ -11,6 +11,13 @@
 #include <glad/glad.h>
 #include <imgui.h>
 
+// TODO: Eventually we want to abstract this Win32 logic into a separate platform layer
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+#include <Windows.h>
+#include <commdlg.h>
+
 Application* Application::s_Instance = nullptr;
 
 Application::Application() : m_DeltaTime(0)
@@ -120,6 +127,32 @@ void Application::PushOverlay(Layer* layer)
 {
 	layer->OnAttach();
 	m_LayerStack.PushOverlay(layer);
+}
+
+std::string Application::OpenFile(const std::string& filter) const
+{
+	OPENFILENAMEA ofn;       // common dialog box structure
+	CHAR szFile[260] = { 0 };       // if using TCHAR macros
+
+	// Initialize OPENFILENAME
+	ZeroMemory(&ofn, sizeof(OPENFILENAME));
+	ofn.lStructSize = sizeof(OPENFILENAME);
+	ofn.hwndOwner = glfwGetWin32Window(static_cast<GLFWwindow*>(m_Window->GetNativeWindowContext()));
+	ofn.lpstrFile = szFile;
+	ofn.nMaxFile = sizeof(szFile);
+	ofn.lpstrFilter = "All\0*.*\0";
+	ofn.nFilterIndex = 1;
+	ofn.lpstrFileTitle = nullptr;
+	ofn.nMaxFileTitle = 0;
+	ofn.lpstrInitialDir = nullptr;
+	ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+	if (GetOpenFileNameA(&ofn) == TRUE)
+	{
+		return ofn.lpstrFile;
+	}
+
+	return std::string();
 }
 
 bool Application::OnWindowResize(WindowResizeEvent& event)
