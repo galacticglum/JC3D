4cc3ca1e1445b1ac7548d6b469940de1b74c56de
GalacticGlum <nerftactic09@gmail.com>
2019-06-13 03:16:09 -0400

    Removed dependency copy from Windows-GenProject.bat

--- Windows-GenProject.bat
+++ Windows-GenProject.bat
@@ -3,9 +3,4 @@ call Windows-Clean.bat
 mkdir Build
 cd Build
 cmake -G "Visual Studio 15 2017 Win64" ..
-mkdir Debug
-mkdir Release
-copy ..\Engine\Dependencies\bin\Debug\x64\ Debug
-copy ..\Engine\Dependencies\bin\Release\x64\ Release
-
 cd ..
